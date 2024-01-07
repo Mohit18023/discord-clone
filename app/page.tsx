889adf778ce1906860fea4f41811ee6768386714
mohit choudhary <mohitchoudhary1054@gmail.com>
2024-01-07 22:31:50 +0530

    nextjs setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="text-3xl text-indigo-500">
+      this is first page
+      <Button>
+        click me!
+      </Button>
+    </div>
+  );
+}
