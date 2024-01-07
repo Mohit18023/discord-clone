c3893507c793db3ed5d482bdc905be7ae7b37285
mohit choudhary <mohitchoudhary1054@gmail.com>
2024-01-07 23:11:18 +0530

    auth folder setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -5,10 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="text-3xl text-indigo-500">
-      this is first page
-      <Button>
-        click me!
-      </Button>
+      This is a Protected Route
     </div>
   );
 }
