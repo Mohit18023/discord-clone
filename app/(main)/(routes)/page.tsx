344778125158c1b9cfde44579c89f622cd97ec36
mohit choudhary <mohitchoudhary1054@gmail.com>
2024-01-09 22:25:24 +0530

    changes made

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="text-3xl text-indigo-500">
-      This is a Protected Route
+      This is a Protected Route in Next js
     </div>
   );
 }
